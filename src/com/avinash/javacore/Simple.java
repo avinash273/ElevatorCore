b36c8fb5cf2f0a5686af82cec1a1dc9f7c19e635
avinash273 <avinash.shanker@mavs.uta.edu>
2021-02-15 22:36:15 -0600

    String[] args

---
+++ src/com/avinash/javacore/Simple.java
@@ -0,0 +1,12 @@
+package com.avinash.javacore;
+
+import java.util.Arrays;
+
+public class Simple {
+    public static void main(String[] args){
+        //you can print command line arguments
+        for(String each : args){
+            System.out.println(each);
+        }
+    }
+}
